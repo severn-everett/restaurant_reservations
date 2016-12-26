1179d26e7f09a096999e70469ec1bcdaeb58a2ac
Severn_Everett <severett@localhost.localdomain>
2016-12-25 22:45:58 -0800

    Initial commit

---
+++ src/main/java/com/severett/RestaurantReservationsApplication.java
@@ -0,0 +1,12 @@
+package com.severett;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestaurantReservationsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestaurantReservationsApplication.class, args);
+	}
+}
