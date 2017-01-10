74d1d33a870a8805a593895da227a077f6497d80
Severn_Everett <severett@localhost.localdomain>
2017-01-09 22:13:01 -0500

    Added log4j2 capability and configuration file; Refined RT repo query to achieve functionality; Added stub web pages for failing to find an open table and failure to reserve

--- src/main/java/com/severett/restaurants/services/RestaurantTableServiceImpl.java
+++ src/main/java/com/severett/restaurants/services/RestaurantTableServiceImpl.java
@@ -5,7 +5,10 @@ import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.Calendar;
 import java.util.Date;
+import java.util.List;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -14,6 +17,9 @@ import com.severett.restaurants.repositories.RestaurantTableRepository;
 
 @Service
 public class RestaurantTableServiceImpl implements RestaurantTableService {
+	
+	private final Logger logger = LoggerFactory.getLogger(this.getClass());
+	private static final DateFormat df = new SimpleDateFormat("yyyy-MM-dd HH:mm");
 
 	@Autowired
 	RestaurantTableRepository restaurantTableRepository;
@@ -25,16 +31,26 @@ public class RestaurantTableServiceImpl implements RestaurantTableService {
 
 	@Override
 	public RestaurantTable getOpenRestaurantTable(Short partySize, String targetTimeString) {
-		DateFormat df = new SimpleDateFormat("MM/dd/yyyy hh:mm");
 		try {
+			
+			logger.info("Searching for a table of {} at {}", partySize, targetTimeString);
 			Date startTime = df.parse(targetTimeString);
 			Calendar calendar = Calendar.getInstance();
 			calendar.setTime(startTime);
 			calendar.set(Calendar.HOUR_OF_DAY, calendar.get(Calendar.HOUR_OF_DAY) + 1);
 			Date endTime = calendar.getTime();
-			return restaurantTableRepository.findBySizeAndTimeWindow(partySize, startTime, endTime);
+			List<RestaurantTable> tableList = restaurantTableRepository.findBySizeAndTimeWindow(partySize, startTime, endTime);
+			if (!tableList.isEmpty()) {
+				return tableList.get(0);
+			} else {
+				return null;
+			}
 		} catch (ParseException pe) {
+			logger.error("Error parsing time '{}': {}", targetTimeString, pe.getMessage());
 			pe.printStackTrace();
+			return null;
+		} catch (Exception e) {
+			logger.error("Error getting restaurant table: ", e);
 			return null;
 		}
 	}
